db44d7a822cad38f1477211103751a8b83554d5e
LAPTOP-UN3TDJJ3\Boweeton <lsoderst@uccs.edu>
2017-11-08 19:36:38 -0700

    Added functionality for Enemy.toString

--- Quest_Enemy_Generator/Quest_Enemy_Generator/Enemy.cs
+++ Quest_Enemy_Generator/Quest_Enemy_Generator/Enemy.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
 
 namespace Quest_Enemy_Generator
 {
@@ -9,7 +11,7 @@ namespace Quest_Enemy_Generator
     {
         #region Fields
 
-
+        public const int ScreenWidth = 84;
 
         #endregion
 
@@ -34,6 +36,7 @@ namespace Quest_Enemy_Generator
 
         public int TotalPDef { get; set; }
         public int TotalGDef { get; set; }
+        public int TotalSDef { get; set; }
         public int XpYield { get; set; }
         public int AveragePlayerLevel { get; set; }
         public int Level { get; set; }
@@ -47,11 +50,133 @@ namespace Quest_Enemy_Generator
         public int WeaponSkill { get; set; }
         public int Scrips { get; set; }
 
+        public bool PrintFullWeapons { get; set; }
+        public bool PrintFullGlyphs { get; set; }
+        public bool PrintArmor { get; set; }
+
         #endregion
 
         #region Methods
 
-        
+        public override string ToString()
+        {
+            // Local Declarations
+            StringBuilder sb = new StringBuilder();
+
+            // Create the partitions
+            string fullPartition = CreateFullPartition();
+            string partialPartition = CreatePartialPartition();
+
+            // Create the info strings
+            List<string> list1 = new List<string>();
+            List<string> list2 = new List<string>();
+
+            list1.Add($" Health: {Health}");
+            list1.Add($"    Dex: {Dex}");
+            list1.Add($"    Acc: {Acc}");
+            list1.Add($"    Str: {Str}");
+            list1.Add($" Percep: {Percep}");
+            list1.Add($"   Snek: {Snek}");
+            list1.Add($"W-Skill: {WeaponSkill}");
+            list1.Add($"M-Skill: {MagicSkill}");
+            list2.Add($"{Level} :Lvl   ");
+            list2.Add($"{XpYield} :XP    ");
+            list2.Add($"{TotalPDef} :P-Def ");
+            list2.Add($"{TotalGDef} :G-Def ");
+            list2.Add(" ");
+            list2.Add(" ");
+            list2.Add(" ");
+            list2.Add($"{Scrips} :Scrips");
+
+            // Print front end stuff
+            sb.AppendLine(fullPartition);
+            sb.AppendLine(TranslateCentered("--[Core Info]--"));
+
+            for (int i = 0; i < list1.Count; i++)
+            {
+                sb.Append(list1[i]);
+                sb.Append(' ', ScreenWidth - list1[i].Length - list2[i].Length);
+                sb.AppendLine(list2[i]);
+            }
+
+            if (PrintFullWeapons)
+            {
+                sb.AppendLine(partialPartition);
+                sb.AppendLine(TranslateCentered("--[Weapons (Full)]--"));
+                foreach (Weapon weapon in Weapons)
+                {
+                    string parry = $"(Parry cost: {weapon.ParryVal})";
+                    sb.AppendLine("----------");
+                    sb.Append(weapon.Name);
+                    sb.Append(' ', ScreenWidth - weapon.Name.Length - parry.Length);
+                    sb.AppendLine(parry);
+                }
+            }
+            else
+            {
+                
+            }
+
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Returns a string with enough spaces in the front so the input strin appear centered
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        string TranslateCentered(string input)
+        {
+            // Local declarations
+            string returnString = "";
+
+            // Calculate the number of spaces
+            int spacesCount = (ScreenWidth - input.Length) / 2;
+
+            // Shove in the spaces
+            for (int i = 0; i < spacesCount; i++)
+            {
+                returnString = $"{returnString} ";
+            }
+
+            // Stick the input string on the end
+            returnString = $"{returnString}{input}";
+
+            return returnString;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        string CreateFullPartition()
+        {
+            // Local declarations
+            StringBuilder bobTheStringBuilder = new StringBuilder();
+
+            // Fill the StringBuilder with the '_' character ScreenWidth times and return it
+            return bobTheStringBuilder.Append('_', ScreenWidth).ToString();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        string CreatePartialPartition()
+        {
+            // Local declarations
+            StringBuilder bobTheStringBuilder = new StringBuilder();
+
+            for (int i = 0; i < (ScreenWidth / 2) - 2; i++)
+            {
+                bobTheStringBuilder.Append("- ");
+            }
+            bobTheStringBuilder.Append("-");
+
+            // Fill the StringBuilder with the '_' character ScreenWidth times and return it
+            return bobTheStringBuilder.ToString();
+        }
 
         #endregion
     }
--- Quest_Enemy_Generator/Quest_Enemy_Generator/Program.cs
+++ Quest_Enemy_Generator/Quest_Enemy_Generator/Program.cs
@@ -7,12 +7,12 @@ namespace Quest_Enemy_Generator
     public class Program
     {
         // Class level declarations
-        public const int ScreenWidth = 130;
+        public const int ScreenWidth = Enemy.ScreenWidth;
 
         static void Main()
         {
             // Resize the console
-            Console.SetWindowSize(ScreenWidth, 30);
+            Console.SetWindowSize(ScreenWidth+1, 30);
             //Console.SetBufferSize(ScreenWidth, 30);
 
             Console.Title = "Quest Enemy Generator";
@@ -45,72 +45,74 @@ namespace Quest_Enemy_Generator
                 }
                 Console.WriteLine($"Calculation time (ms): {sw.ElapsedTicks / 10000f:0.0000}");
 
-                const int HeaderPartitioning = 17;
+                Console.WriteLine(dm.Enemy.ToString());
 
-                Console.WriteLine(TranslateCentered("[Enemy]"));
-                Console.Write($"Lvl: {dm.Enemy.Level,-HeaderPartitioning}");
-                Console.Write($"// {dm.Enemy.Difficulty,-HeaderPartitioning}");
-                Console.Write($"// {dm.Enemy.GameClass.Name,-HeaderPartitioning}");
-                Console.Write($"// {dm.Enemy.Race.Name,-HeaderPartitioning}");
-                Console.WriteLine();
+                //const int HeaderPartitioning = 17;
 
-                Console.WriteLine($"Kill XP: {dm.Enemy.XpYield,-4} Ast XP: {(dm.Enemy.XpYield + 1)/2}");
+                //Console.WriteLine(TranslateCentered("[Enemy]"));
+                //Console.Write($"Lvl: {dm.Enemy.Level,-HeaderPartitioning}");
+                //Console.Write($"// {dm.Enemy.Difficulty,-HeaderPartitioning}");
+                //Console.Write($"// {dm.Enemy.GameClass.Name,-HeaderPartitioning}");
+                //Console.Write($"// {dm.Enemy.Race.Name,-HeaderPartitioning}");
+                //Console.WriteLine();
 
-                Console.WriteLine(partialPartition);
+                //Console.WriteLine($"Kill XP: {dm.Enemy.XpYield,-4} Ast XP: {(dm.Enemy.XpYield + 1)/2}");
 
-                const int Partition = 8;
+                //Console.WriteLine(partialPartition);
 
-                Console.WriteLine(TranslateCentered("[Core Stats]"));
-                Console.WriteLine($"{"Health:",+Partition} {dm.Enemy.Health}");
-                Console.WriteLine($"{"Dex:",+Partition} {dm.Enemy.Dex}");
-                Console.WriteLine($"{"Acc:",+Partition} {dm.Enemy.Acc}");
-                Console.WriteLine($"{"Str:",+Partition} {dm.Enemy.Str}");
-                Console.WriteLine($"{"Percep:",+Partition} {dm.Enemy.Percep}");
-                Console.WriteLine($"{"Snek:",+Partition} {dm.Enemy.Snek}");
-                Console.WriteLine($"{"W-Skill:",+Partition} {dm.Enemy.WeaponSkill}");
-                Console.WriteLine($"{"M-Skill:",+Partition} {dm.Enemy.MagicSkill}");
+                //const int Partition = 8;
+
+                //Console.WriteLine(TranslateCentered("[Core Stats]"));
+                //Console.WriteLine($"{"Health:",+Partition} {dm.Enemy.Health}");
+                //Console.WriteLine($"{"Dex:",+Partition} {dm.Enemy.Dex}");
+                //Console.WriteLine($"{"Acc:",+Partition} {dm.Enemy.Acc}");
+                //Console.WriteLine($"{"Str:",+Partition} {dm.Enemy.Str}");
+                //Console.WriteLine($"{"Percep:",+Partition} {dm.Enemy.Percep}");
+                //Console.WriteLine($"{"Snek:",+Partition} {dm.Enemy.Snek}");
+                //Console.WriteLine($"{"W-Skill:",+Partition} {dm.Enemy.WeaponSkill}");
+                //Console.WriteLine($"{"M-Skill:",+Partition} {dm.Enemy.MagicSkill}");
                 
 
-                Console.WriteLine(TranslateCentered("[Weapons]"));
-                foreach (Weapon currentWeapon in dm.Enemy.Weapons)
-                {
-                    Console.WriteLine(partialPartition);
-                    Console.WriteLine($"{currentWeapon.DisplayName,-35}(Parry Cost: {currentWeapon.ParryVal})");
-                    Console.WriteLine($"{"Name",-NameToDescriptionOffset}{"Description",-DescriptionTpSpeedOffset}{"SPD",-SpeedToDamageOffset}{"DMG"}");
-                    foreach (WeaponMove currentMove in currentWeapon.WeaponMoves)
-                    {
-                        Console.WriteLine($"{currentMove.Name,-NameToDescriptionOffset}{currentMove.Description,-DescriptionTpSpeedOffset}{currentMove.Speed,-SpeedToDamageOffset}{currentMove.Damage}");
-                    }
-                }
+                //Console.WriteLine(TranslateCentered("[Weapons]"));
+                //foreach (Weapon currentWeapon in dm.Enemy.Weapons)
+                //{
+                //    Console.WriteLine(partialPartition);
+                //    Console.WriteLine($"{currentWeapon.DisplayName,-35}(Parry Cost: {currentWeapon.ParryVal})");
+                //    Console.WriteLine($"{"Name",-NameToDescriptionOffset}{"Description",-DescriptionTpSpeedOffset}{"SPD",-SpeedToDamageOffset}{"DMG"}");
+                //    foreach (WeaponMove currentMove in currentWeapon.WeaponMoves)
+                //    {
+                //        Console.WriteLine($"{currentMove.Name,-NameToDescriptionOffset}{currentMove.Description,-DescriptionTpSpeedOffset}{currentMove.Speed,-SpeedToDamageOffset}{currentMove.Damage}");
+                //    }
+                //}
 
-                if (dm.Enemy.GameClass.CanUseMagic)
-                {
-                    Console.WriteLine(partialPartition);
-                    Console.WriteLine(TranslateCentered("[Magic stuffs]"));
-                    foreach (Glyph enemyGlyph in dm.Enemy.Glyphs)
-                    {
-                        Console.WriteLine($"{enemyGlyph.Name, -25}: \"{enemyGlyph.Description}\"");
-                    }
-                }
-                const int TypeOffset = 10;
-                const int NameOffset = 25;
-                Console.WriteLine(partialPartition);
-                Console.WriteLine(TranslateCentered("[Armors]"));
-                Console.WriteLine($"{"Type",-TypeOffset}{"Name",-NameOffset}DefVal");
-                foreach (Armor arm in dm.Enemy.Armors)
-                {
-                    Console.WriteLine($"{arm.AType.ToString(),-TypeOffset}{arm.Name,-NameOffset}{arm.DefVal}");
-                }
+                //if (dm.Enemy.GameClass.CanUseMagic)
+                //{
+                //    Console.WriteLine(partialPartition);
+                //    Console.WriteLine(TranslateCentered("[Magic stuffs]"));
+                //    foreach (Glyph enemyGlyph in dm.Enemy.Glyphs)
+                //    {
+                //        Console.WriteLine($"{enemyGlyph.Name, -25}: \"{enemyGlyph.Description}\"");
+                //    }
+                //}
+                //const int TypeOffset = 10;
+                //const int NameOffset = 25;
+                //Console.WriteLine(partialPartition);
+                //Console.WriteLine(TranslateCentered("[Armors]"));
+                //Console.WriteLine($"{"Type",-TypeOffset}{"Name",-NameOffset}DefVal");
+                //foreach (Armor arm in dm.Enemy.Armors)
+                //{
+                //    Console.WriteLine($"{arm.AType.ToString(),-TypeOffset}{arm.Name,-NameOffset}{arm.DefVal}");
+                //}
 
-                Console.WriteLine(TranslateCentered($"TOTAL PDef: {dm.Enemy.TotalPDef,-10} TOTAL GDef: {dm.Enemy.TotalGDef}"));
+                //Console.WriteLine(TranslateCentered($"TOTAL PDef: {dm.Enemy.TotalPDef,-10} TOTAL GDef: {dm.Enemy.TotalGDef}"));
 
 
-                Console.WriteLine(partialPartition);
-                Console.WriteLine(TranslateCentered("[Loot]"));
-                Console.WriteLine($"Scrips: {dm.Enemy.Scrips}");
+                //Console.WriteLine(partialPartition);
+                //Console.WriteLine(TranslateCentered("[Loot]"));
+                //Console.WriteLine($"Scrips: {dm.Enemy.Scrips}");
 
-                Console.WriteLine(fullPartition);
-                Console.WriteLine(fullPartition);
+                //Console.WriteLine(fullPartition);
+                //Console.WriteLine(fullPartition);
             }
             average = average / (Repeats - 1);
             Console.WriteLine($"Average calculation time (ms): {average / 10000f:0.0000}");
