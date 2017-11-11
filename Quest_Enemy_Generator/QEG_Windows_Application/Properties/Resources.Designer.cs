ad9ed885782063c58573a8edc3b5f608b20e4eb5
Luke <luke.r.strom@gmail.com>
2017-11-10 21:04:17 -0700

    Added EVEN NEWER icon

--- Quest_Enemy_Generator/QEG_Windows_Application/Properties/Resources.Designer.cs
+++ Quest_Enemy_Generator/QEG_Windows_Application/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace QEG_Windows_Application.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap u9Yujsc {
-            get {
-                object obj = ResourceManager.GetObject("u9Yujsc", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
